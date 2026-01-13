a5ac100fcf3c870082cd2b89b4097f7eb7970b0e
KingGeorge9 <mrjorgeobika@gmail.com>
2026-01-13 12:41:58 +0100

    Remove CompanyLogos section from Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,6 @@ import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
 import Generating from "./Generating";
 import Notification from "./Notification";
-import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -92,8 +91,6 @@ const Hero = () => {
 
           <BackgroundCircles />
         </div>
-
-        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
       </div>
 
       <BottomLine />
